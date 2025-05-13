e66528b54936c38e32475ce6f80a70514abbc980
plxl <210124508+plxl@users.noreply.github.com>
2025-05-13 21:48:14 +0800

    UPDATE Window starts in Center of screen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -690,6 +690,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "mpegui";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numAudioGain)).EndInit();
