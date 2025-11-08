e43b4a103e710fb171b1ec87446ddc2a71116814
plxl <210124508+plxl@users.noreply.github.com>
2025-11-08 16:06:51 +0800

    FIX FileConversionInfo.Clone() method not cloning Audio Normalise boolean

--- Classes/FileConversionInfo.cs
+++ Classes/FileConversionInfo.cs
@@ -56,6 +56,7 @@ namespace mpegui
             clone.AudioGain = AudioGain;
             clone.AudioUseDb = AudioUseDb;
             clone.AudioDelaySeconds = AudioDelaySeconds;
+            clone.AudioNormalise = AudioNormalise;
             clone.CropFilter = CropFilter;
             clone.Encoder = Encoder;
             clone.Tags = Tags;
