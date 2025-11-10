ddba34ebbb08221b8c10010cee6f803a44f0917b
plxl <210124508+plxl@users.noreply.github.com>
2025-11-10 18:48:22 +0800

    ADD basic FadeInfo class to be used for Video/Audio fading

---
+++ Classes/FadeInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mpegui.Classes
+{
+    internal enum FadeMode
+    {
+        None,
+        Video,
+        Audio,
+        Both,
+    }
+    internal class FadeInfo
+    {
+        public FadeMode Mode { get; set; }
+        public int Milliseconds { get; set; }
+
+        public FadeInfo(FadeMode mode, int milliseconds)
+        {
+            Mode = mode;
+            Milliseconds = milliseconds;
+        }
+    }
+}
